6e25395a57c5897007806462731f625a8adf7e72
Harsh Pendhari <harshpendhari2901@gmail.com>
2026-01-28 21:41:49 +0530

    Create reading_JSON_file.py

---
+++ Pandas/reading_JSON_file.py
@@ -0,0 +1,3 @@
+"""Big data sets are often stored, or extracted as JSON.
+JSON is plain text, but has the format of an object, and is well known in the world of programming, including Pandas.
+In our examples we will be using a JSON file called 'data.json'."""
